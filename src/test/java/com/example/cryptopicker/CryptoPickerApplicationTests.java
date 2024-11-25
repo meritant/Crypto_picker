e69180551396eb87c8bb6b8e5f981ceb262c9128
User User <useruser@192.168.1.101>
2024-11-25 23:22:36 +0300

    project setup

---
+++ src/test/java/com/example/cryptopicker/CryptoPickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptopicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoPickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
